0a5e67560e24fd032da65aca15ab40f0fd2a08a7
Kyle Geary <Kgeary09@gmail.com>
2019-07-03 00:25:23 -0500

    start mdx integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
+        `gatsby-mdx`,
         `gatsby-plugin-netlify-cms`,
     ],
 };
